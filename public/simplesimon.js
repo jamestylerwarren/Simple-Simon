5d448e9b3a3c0d6520da0050f39f80b61651f1c0
Tyler Warren <warrejt@yahoo.com>
2016-06-21 15:52:08 -0500

    simon says js

--- public/simplesimon.js
+++ public/simplesimon.js
@@ -1,31 +1,51 @@
-var solution = [];
-var buttons = $('.buttons');
-var index = 0;
+(function () { 
+    "use strict"
 
-function randomNumber(amount) {
-    var random = Math.floor(Math.random() * (amount) + 1);
-    return random;
-};
 
-buttons.each(function() {
-    $(this).click(function() {
-        if (solution[index] == $(this).data('value')) {
-            index++;
-            console.log("success.")
+    var solution = [];
+    var index = 0;
+    var level = 0;
+    var buttons = $('.buttons');
+
+    //------- Generate a random number between 1 - 4 --------------------// 
+    function randomNumber (amount) {
+            var random = Math.floor(Math.random() * (amount) + 1);
+            return random;
+        };
+
+
+    //------- Upon start generate random number and push to solution------//
+    $('#start').click(function() {
+            solution = [];
             solution.push(randomNumber(4));
             console.log(solution);
+        });
 
-        } else {
-            index = 0;
-            solution = [];
-            console.log("failure.")
-        }
-    });
-});
 
-$('#start').click(function() {
-    solution = [];
-    index = 0;
-    solution.push(randomNumber(4));
-    console.log(solution);
-});
+    //------- Listeners for colored buttons ------------------------------//
+    var listenerButtons = buttons
+    for (var i = 0; i < listenerButtons.length; i++) {
+        listenerButtons[i].addEventListener('click', function(){
+            if (solution[index] == $(this).data('number')) {
+                index++;
+                solution.push(randomNumber(4));
+                console.log(solution);
+            } else {
+                solution = [];
+                index = 0;
+                console.log('you failed');
+            }
+
+
+        });
+    }
+
+
+
+
+
+
+
+
+
+})();
